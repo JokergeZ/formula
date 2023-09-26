6519e23d3654588def739820494f45eb8f3927f1
Rudolf Iohim <r.hahahaha@corp.nstu.ru>
2023-09-26 21:55:43 +0700

    formula.1

---
+++ formula.1.py
@@ -0,0 +1,9 @@
+import math
+
+a = float(input("Введите ваше число: "))
+
+z1 = ((math.sin(2*a))+(math.sin(5*a))-(math.sin(3*a)))/((math.cos(a))-(math.cos(3*a))+(math.cos(5*a)))
+z2 = (math.tan(3*a))
+
+print (z1)
+print (z2)
